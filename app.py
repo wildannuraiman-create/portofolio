05ab2b19baa9b17a9a9df2de0f233230de83dea0
Wildan Aiman <wildannuraiman@gmail.com>
2025-12-24 20:48:21 +0700

    streamlit

--- app.py
+++ app.py
@@ -3,14 +3,9 @@ import streamlit as st
 st.set_page_config(
     page_title="Portofolio Data Science Saya",
     page_icon="✨",
-    layout="wide", # Bisa 'centered' atau 'wide'
-    initial_sidebar_state="expanded" # Bisa 'auto', 'expanded', 'collapsed'
+    layout="wide",
+    initial_sidebar_state="expanded"
 )
 
 st.title("Selamat Datang di Portofolio Saya!")
-# Konten aplikasi lainnya
-
-
-
-
-
+st.write("Ini adalah aplikasi Streamlit pertama saya.")
